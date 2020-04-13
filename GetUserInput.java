10877ca4fdc74c20276cc2a9f6483068a9a3a323
nhiblar <46576774+nhiblar@users.noreply.github.com>
2020-04-13 16:35:47 -0700

    Add files via upload

---
+++ GetUserInput.java
@@ -0,0 +1,25 @@
+package Minesweeper;
+
+import java.util.Scanner;
+
+public class GetUserInput {
+
+	public static int getSizeFromUser() {
+		
+		int choice = 0;
+		do {
+			Scanner kb = new Scanner(System.in);
+			String input = kb.nextLine();
+			try {
+				choice = Integer.parseInt(input); 
+			}catch(NumberFormatException e) {
+				System.out.println("input is not an int value"); 
+			} 
+
+		}while(choice < 1 || choice > 100);
+		
+		return choice;
+	}
+
+
+}
