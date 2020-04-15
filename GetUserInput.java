73fc06a232513452f0daa3a52d258c33f6a16f13
nhiblar <46576774+nhiblar@users.noreply.github.com>
2020-04-15 16:23:27 -0700

    Add files via upload

--- GetUserInput.java
+++ GetUserInput.java
@@ -7,19 +7,16 @@ import java.util.Scanner;
 public class GetUserInput {
 
 	public static int getSizeFromUser() {
-		
-		int choice = 0;
-		do {
-			Scanner kb = new Scanner(System.in);
-			String input = kb.nextLine();
-			try {
-				choice = Integer.parseInt(input); 
-			}catch(NumberFormatException e) {
-				System.out.println("input is not an int value"); 
-			} 
 
-		}while(choice < 1 || choice > 100);
-		
+		int choice = 0;
+		Scanner kb = new Scanner(System.in);
+		String input = kb.nextLine();
+		try {
+			choice = Integer.parseInt(input); 
+		}catch(NumberFormatException e) {
+			System.out.println("input is not an int value"); 
+		} 
+
 		return choice;
 	}
 
