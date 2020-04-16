ce9238689ec7f1a5de436e943539aa653d6993f6
nhiblar <46576774+nhiblar@users.noreply.github.com>
2020-04-16 09:40:49 -0700

    Update Minesweeper.java
    updateds to match coding standards

--- Minesweeper.java
+++ Minesweeper.java
@@ -12,12 +12,10 @@ public class Minesweeper {
 	public static void main(String[] args) throws IOException {
 		
 		Scanner kb = new Scanner(System.in);
-		//BufferedReader br = new BufferedReader(new FileReader("minesTester.txt"));
 		int row, col;
 		int count = 1;
 		
 		do{
-			//String str = br.readLine();
 			String str = kb.nextLine();
 			String[] strArrRowAndCol = str.split(" ");
 			row = Integer.parseInt(String.valueOf(strArrRowAndCol[0]));
@@ -32,8 +30,8 @@ public class Minesweeper {
 				str = kb.nextLine();
 				for(int j = 0; j < col; j++) {
 					board[i][j] = str.charAt(j);
-				}
-			}//end outer for
+				}// end for loop j
+			}//end for loop i
 			
 			solve(board);
 			printBoard(board);
@@ -42,16 +40,16 @@ public class Minesweeper {
 
 		}while(row != 0 && col != 0);
 	
-	}//end main
+	}
 
 	private static void printBoard(char[][] board) {
 
 		for(int i = 0; i < board.length; i++) {	      
 			for(int j = 0; j < board[i].length; j++) {
 				System.out.print(board[i][j] + " ");
-			}
+			}//end for loop j
 			System.out.println();
-		}
+		}//end for loop i
 
 	}
 
@@ -88,8 +86,8 @@ public class Minesweeper {
 
 					board[i][j] = Character.forDigit(count,10);
 				}//end if
-			}//end inner for
-		}//end outer for
-	}//end solve
+			}//end for loop j
+		}//end for loop i
+	}
 
-}//end Driver
+}
