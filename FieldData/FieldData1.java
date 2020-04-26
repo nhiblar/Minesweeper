12d5e996b89351ceddcdae92e34848116235f247
nhiblar <46576774+nhiblar@users.noreply.github.com>
2020-04-25 20:21:46 -0700

    hard-coded solutions
    Each file is a solution to one field in the minesTester.txt. to be used in the JUnit tests for HW2.

---
+++ FieldData/FieldData1.java
@@ -0,0 +1,8 @@
+package FieldData;
+
+public class FieldData1 {
+	
+	public static final char[][] field1 = {{'0'}};
+
+}
+ 
---
+++ FieldData/FieldData2.java
@@ -0,0 +1,7 @@
+package FieldData;
+
+public class FieldData2{
+	
+	public static final char[][] field2 = {{'*'}};
+
+}
